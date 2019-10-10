e82a8e3862576f6d34331351a3ec308efcf5f9f7
Vijay S Deshmukh <vj_deshmukh@yahoo.com>
2019-10-10 16:37:32 -0400

    initial

---
+++ cicd-testing-demo.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace cicd_testing_demo.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // check if math sum i correct
+            Assert.Fail();
+
+        }
+         
+    }
+}
